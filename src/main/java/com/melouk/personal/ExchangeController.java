b0781fb98dfb2fa9e04195ba8b60af819cf02897
Mazen Melouk <melouk.mazen@gmail.com>
2024-01-21 17:36:25 +0100

    Add external call to fetch rates. (#3)
    Uses https://v6.exchangerate-api.com/v6 as an external source for conversion rates via Spring RestTemplate.
    
    No special handling or resilience have been added.

--- src/main/java/com/melouk/personal/ExchangeController.java
+++ src/main/java/com/melouk/personal/ExchangeController.java
@@ -1,15 +1,29 @@
 package com.melouk.personal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class ExchangeController {
+    @Autowired
+    private RestTemplate restTemplate;
+    @Value("${API_TOKEN:dummy_token}")
+    private String token;
 
     @GetMapping("/convert")
     public ConversionResult convert(
@@ -20,7 +34,19 @@ public class ExchangeController {
         if (amount <= 0) {
             throw new IllegalArgumentException("Parameter 'amount' needs to be greater than 0.");
         }
-        return new ConversionResult(from, to, ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")), amount, amount * 1.1);
+        var uri = String.format("https://v6.exchangerate-api.com/v6/%s/latest/%s", token, from);
+        var rateResponse = restTemplate.getForObject(uri, ExchangeRateExternalResponse.class);
+        var ratesMap = Optional.ofNullable(rateResponse)
+                .map(ExchangeRateExternalResponse::conversionRates)
+                .orElseThrow(() -> new IllegalStateException(String.format("Could not find rates for %s as a source currency.", from)));
+        var rateValue = Optional.ofNullable(ratesMap.get(to))
+                .orElseThrow(() -> new IllegalStateException(String.format("Could not find rates for %s as a target currency.", to)));
+        var epochTimestamp = Optional.of(rateResponse)
+                .map(ExchangeRateExternalResponse::timeLastUpdateUnix)
+                .map(Instant::ofEpochSecond)
+                .map(instant -> ZonedDateTime.ofInstant(instant, ZoneId.of("UTC")))
+                .orElseGet(() -> ZonedDateTime.now().withZoneSameInstant(ZoneId.of("UTC")));
+        return new ConversionResult(from, to, epochTimestamp, amount, amount * rateValue);
     }
 
     @ControllerAdvice
@@ -28,14 +54,33 @@ public class ExchangeController {
 
         @ExceptionHandler({MissingServletRequestParameterException.class, IllegalArgumentException.class})
         @ResponseStatus(HttpStatus.BAD_REQUEST)
-        public ResponseEntity<Problem> handleException(Exception e) {
+        public ResponseEntity<Problem> handleInputException(Exception e) {
             return new ResponseEntity<>(new Problem(e.getMessage(), HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
         }
+
+        @ExceptionHandler(IllegalStateException.class)
+        @ResponseStatus(HttpStatus.NOT_FOUND)
+        public ResponseEntity<Problem> handleNotFoundException(Exception e) {
+            return new ResponseEntity<>(new Problem(e.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+        }
+
+        @ExceptionHandler(Exception.class)
+        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        public ResponseEntity<Problem> handleOtherException(Exception e) {
+            return new ResponseEntity<>(new Problem("Something wrong happen, contact a specialist!",
+                    HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 
     public record ConversionResult(String from, String to, ZonedDateTime timestamp, double original, double converted) {
     }
 
     public record Problem(String message, int code) {
     }
+
+    public record ExchangeRateExternalResponse(@JsonProperty("time_last_update_unix") Long timeLastUpdateUnix,
+                                               @JsonProperty("conversion_rates") Map<String, Double> conversionRates) {
+    }
 }
+
--- src/test/java/com/melouk/personal/ExchangeControllerTest.java
+++ src/test/java/com/melouk/personal/ExchangeControllerTest.java
@@ -1,5 +1,7 @@
 package com.melouk.personal;
 
+import com.melouk.personal.ExchangeController.ExchangeRateExternalResponse;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -7,27 +9,40 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
 
 @WebMvcTest(ExchangeController.class)
 class ExchangeControllerTest {
     private static final String TIMESTAMP_PATTERN = "^(?:[1-9]\\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\\d|2[0-3]):[0-5]\\d:[0-5]\\d(?:\\.\\d{1,9})?(?:Z|[+-][01]\\d:[0-5]\\d)$";
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private RestTemplate restTemplate;
 
-    @Test
-    void testConvertIsSuccessful() throws Exception {
+    @ParameterizedTest(name = "Timestamp is {0}")
+    @MethodSource
+    void testConvertIsSuccessful(Long epoch, Matcher<?> timestampMatcher) throws Exception {
+        String uri = "https://v6.exchangerate-api.com/v6/dummy_token/latest/USD";
+        when(restTemplate.getForObject(uri, ExchangeRateExternalResponse.class))
+                .thenReturn(new ExchangeRateExternalResponse(epoch, Map.of("EUR", 1.1)));
+
         mockMvc.perform(MockMvcRequestBuilders.get("/convert")
                         .param("from", "USD")
                         .param("to", "EUR")
@@ -39,7 +54,76 @@ class ExchangeControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.to", equalTo("EUR")))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.original", equalTo(10.0)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.converted", equalTo(11.0)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp", matchesPattern(TIMESTAMP_PATTERN)));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp", timestampMatcher));
+    }
+
+    static Stream<Arguments> testConvertIsSuccessful() {
+        var timestamp = ZonedDateTime.of(2024, 1, 1,
+                        0, 0, 0, 0,
+                        ZoneId.of("UTC"))
+                .format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        return Stream.of(
+                Arguments.of(1704067200L, equalTo(timestamp)),
+                Arguments.of(null, matchesPattern(TIMESTAMP_PATTERN))
+        );
+    }
+
+    @Test
+    void testConvertFailsToFindSourceCurrency() throws Exception {
+        String uri = "https://v6.exchangerate-api.com/v6/dummy_token/latest/USD";
+        when(restTemplate.getForObject(uri, ExchangeRateExternalResponse.class)).thenReturn(null);
+        mockMvc.perform(MockMvcRequestBuilders.get("/convert")
+                        .param("from", "USD")
+                        .param("to", "EUR")
+                        .param("amount", "10"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", equalTo(404)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", equalTo("Could not find rates for USD as a source currency.")));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testConvertFailsToFindTargetCurrency(Map<String, Double> rates) throws Exception {
+        String uri = "https://v6.exchangerate-api.com/v6/dummy_token/latest/USD";
+        when(restTemplate.getForObject(uri, ExchangeRateExternalResponse.class))
+                .thenReturn(new ExchangeRateExternalResponse(1704067200L, rates));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/convert")
+                        .param("from", "USD")
+                        .param("to", "EUR")
+                        .param("amount", "10"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", equalTo(404)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", equalTo("Could not find rates for EUR as a target currency.")));
+    }
+
+    static Stream<Arguments> testConvertFailsToFindTargetCurrency() {
+        return Stream.of(
+                        Map.of(),
+                        Map.of("JPY", 1.2)
+                )
+                .map(Arguments::of);
+    }
+
+    @Test
+    void testConvertFailsToMakeExternalCall() throws Exception {
+        String uri = "https://v6.exchangerate-api.com/v6/dummy_token/latest/USD";
+        when(restTemplate.getForObject(uri, ExchangeRateExternalResponse.class))
+                .thenThrow(RuntimeException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/convert")
+                        .param("from", "USD")
+                        .param("to", "EUR")
+                        .param("amount", "10"))
+                .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.*", hasSize(2)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", equalTo(500)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", equalTo("Something wrong happen, contact a specialist!")));
     }
 
     @ParameterizedTest
