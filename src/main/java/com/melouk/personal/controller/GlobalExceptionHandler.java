cc6ac5c035e1abc77875384832bb5961c0d58cfd
mazenmelouk <melouk.mazen@gmail.com>
2024-01-21 17:53:50 +0100

    Repackaging and refactoring ExchangeController
    Extract the GlobalExceptionHandler and add some packages

---
+++ src/main/java/com/melouk/personal/controller/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.melouk.personal.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MissingServletRequestParameterException.class, IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExchangeController.Problem> handleInputException(Exception e) {
+        return new ResponseEntity<>(new ExchangeController.Problem(e.getMessage(), HttpStatus.BAD_REQUEST.value()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ExchangeController.Problem> handleNotFoundException(Exception e) {
+        return new ResponseEntity<>(new ExchangeController.Problem(e.getMessage(), HttpStatus.NOT_FOUND.value()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ExchangeController.Problem> handleOtherException(Exception e) {
+        return new ResponseEntity<>(new ExchangeController.Problem("Something wrong happen, contact a specialist!",
+                HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
