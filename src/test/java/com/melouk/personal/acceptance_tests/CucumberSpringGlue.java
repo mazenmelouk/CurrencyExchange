dfe28d3251a62838d13a6e653660cee63bc5395a
mazenmelouk <melouk.mazen@gmail.com>
2024-02-17 11:28:16 +0100

    Setup Cucumber for BDD acceptance tests
    Adds Cucumber dependencies with Spring integration and maven configuration to be part of the build.
    
    Having tests being part of the build required using an in-memory database to simplify github builds.
    
    I also added a test for the health endpoint to verify cucumber runs.

---
+++ src/test/java/com/melouk/personal/acceptance_tests/CucumberSpringGlue.java
@@ -0,0 +1,13 @@
+package com.melouk.personal.acceptance_tests;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
+public class CucumberSpringGlue {
+}
